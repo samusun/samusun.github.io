6f94bc5306b4998bef15413655ca7fa8f1fca9cd
samusun <samuelsinkorg@gmail.com>
2021-02-04 16:56:04 +0100

    Testar uppladdning

---
+++ precache-manifest.22d743ce097ab6d7e6b593644896fd44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0ca8516f407c140192",
+    "url": "/css/app.6e84c3af.css"
+  },
+  {
+    "revision": "9051d71cc7b4ec3341ea1e40ac270ccc",
+    "url": "/img/logos.9051d71c.png"
+  },
+  {
+    "revision": "0c0db01ed24463e51fac3eea0fde08eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d0ca8516f407c140192",
+    "url": "/js/app.615d047c.js"
+  },
+  {
+    "revision": "3864377ea96d8adaa61b",
+    "url": "/js/chunk-vendors.378398ea.js"
+  },
+  {
+    "revision": "bbf0168b22456ef15dd6fec46122b998",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.22d743ce097ab6d7e6b593644896fd44.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "mr-hindsight"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute("https://api.coingecko.com/api/v3/search/trending", new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":5, plugins: [] }), 'GET');
